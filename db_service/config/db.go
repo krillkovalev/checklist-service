f22beb47eaa43f4831b6af81e12b60094a5dd050
krillkovalev <kovalyov201524@gmail.com>
2025-01-20 17:31:42 +0300

    добавлено чтение credentials из .env файла

--- db_service/config/db.go
+++ db_service/config/db.go
@@ -2,12 +2,25 @@ package config
 
 import (
 	"database/sql"
-	_ "github.com/lib/pq"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func ConnectPostgresDB() (*sql.DB, error) {
-	connstr := "user=krillkovalev dbname=checklist_info password=108814 host=localhost port=5432 sslmode=disable"
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Failed to load .env file")
+	}
+	pgUser := os.Getenv("DB_USER")
+	dbName := os.Getenv("DB_NAME")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	connstr := fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%s sslmode=disable", pgUser, dbName, dbPass, dbHost, dbPort)
 	db, err := sql.Open("postgres", connstr)
 	if err != nil {
 		return nil, fmt.Errorf("unable to connect to db: %v", err)
--- db_service/config/redis.go
+++ db_service/config/redis.go
@@ -2,20 +2,26 @@ package config
 
 import (
 	"context"
-
 	"github.com/redis/go-redis/v9"
+    "os"
+    "log"
+	"github.com/joho/godotenv"
 )
 
 func RedisConnection(ctx context.Context) *redis.Client{
-
+    err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Failed to load .env file")
+	}
+    address := os.Getenv("REDIS_ADDR")
 	client := redis.NewClient(&redis.Options{
-        Addr:     "localhost:6379",
+        Addr:     address,
         Password: "", // no password set
         DB:       0,  // use default DB
     })
 
     // Ping Redis to check if the connection is working
-    _, err := client.Ping(ctx).Result()
+    _, err = client.Ping(ctx).Result()
     if err != nil {
         panic(err) 
     }
