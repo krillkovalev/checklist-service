fda767c0c9a6c0ca459ebb8d83409314cf150218
krillkovalev <kovalyov201524@gmail.com>
2025-01-24 12:11:58 +0300

    Переписал на потребление всех партиций консьюмером

--- kafka_service/main.go
+++ kafka_service/main.go
@@ -8,13 +8,21 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-
 	"github.com/IBM/sarama"
 )
 
 func main() {
 
-	topic := "tasks-log-topic"
+	file, err := os.OpenFile("KafkaServiceLogs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	topic := os.Getenv("KAFKA_TOPIC")
+	if topic == "" {
+		topic = "tasks-log-topic"
+	}
 
 	hostName := os.Getenv("KAFKA_HOST")
 	if hostName == "" {
@@ -26,37 +34,45 @@ func main() {
 		log.Fatalf("unable to connect to the topic: %v", err)
 	}
 
-	consumer, err := worker.ConsumePartition(topic, 0, sarama.OffsetOldest)
-	if err != nil {
-		log.Fatalf("error while consuming: %v", err)
-	}
-
 	fmt.Println("Server kafka_service is running")
 
-	sigchan := make(chan os.Signal, 1)
-	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 
-	doneCh := make(chan struct{})
-	go func() {
-		for {
-			select {
-			case err := <- consumer.Errors():
-				fmt.Println(err)
-			case msg := <- consumer.Messages():
-				utils.WriteToLog(msg.Value)
-				if err != nil {
-					fmt.Println(err)
-				}
-			case <- sigchan:
-				fmt.Println("Interrupt is detected")
-				doneCh <- struct{}{}
-			}
+	partitions, err := worker.Partitions(topic)
+	if err != nil {
+		log.Fatalf("error getting partitions: %v", err)
+	}
+
+	for _, partition := range partitions {
+		consumer, err := worker.ConsumePartition(topic, partition, sarama.OffsetOldest)
+		if err != nil {
+			log.Fatalf("error consuming partitions: %v", err)
 		}
-	}()
 
-	<- doneCh
-	err = worker.Close(); if err != nil {
-		log.Fatalf("error while consuming: %v", err)
+		sigchan := make(chan os.Signal, 1)
+		signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+		doneCh := make(chan struct{})
+		go func(consumer sarama.PartitionConsumer) {
+			for {
+				select {
+				case err := <- consumer.Errors():
+					fmt.Println(err)
+				case msg := <- consumer.Messages():
+					utils.WriteToLog(file, msg.Value)
+					if err != nil {
+						fmt.Println(err)
+					}
+				case <- sigchan:
+					fmt.Println("Interrupt is detected")
+					doneCh <- struct{}{}
+				}
+			}
+		}(consumer)
+
+		<- doneCh
+		err = worker.Close(); if err != nil {
+			log.Fatalf("error while consuming: %v", err)
+		}
 	}
 
 }
--- kafka_service/utils/utils.go
+++ kafka_service/utils/utils.go
@@ -3,28 +3,20 @@ package utils
 import (
 	"fmt"
 	"kafka_service/models"
-	"log"
 	"encoding/json"
 	"os"
 )
 
-func WriteToLog(data []byte) error {
+func WriteToLog(file *os.File, data []byte) error {
 	msg := new(models.Messsage)
 	err := json.Unmarshal(data, &msg)
     if err != nil {
         return err
     }
 
-	file, err := os.OpenFile("KafkaServiceLogs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatal(err)
-	}
+	
 	str := fmt.Sprintf("INFO: %s %s", msg.Timestamp, msg.Action)
 	_, err = fmt.Fprintln(file, str)
-	if err != nil {
-		return err
-	}
-	err = file.Close()
 	if err != nil {
 		return err
 	}
